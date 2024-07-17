4927de4be42a847b2336fd8ff386810bf708ae97
Gero Posmyk-Leinemann <gero@gitpod.io>
2024-07-17 06:52:02 +0000

    ignore non-PIDs

--- record.sh
+++ record.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Delay of 5 minutes (300 seconds)
-sleep 1
+sleep 5
 
 # Function to start JFR for each JVM process
 start_jfr_sessions() {
     jps | while read -r line; do
         pid=$(echo $line | awk '{print $1}')
-        if [ -n "$pid" ]; then
+        if [[ $pid =~ ^[0-9]+$ ]]; then
             jfr_dir="/workspace/jvm_debug/jfr-$pid"
             mkdir -p $jfr_dir
             timestamp=$(date +"%Y%m%d_%H%M%S")
-            jcmd $pid JFR.start duration=60s filename="$jfr_dir/recording_$timestamp.jfr"
+            jcmd $pid JFR.start duration=10s filename="$jfr_dir/recording_$timestamp.jfr"
         fi
     done
 }
--- tracer.sh
+++ tracer.sh
@@ -5,8 +5,8 @@
 get_jvm_stack_traces() {
     jps | while read -r line; do
         pid=$(echo $line | awk '{print $1}')
-        if [ -n "$pid" ]; then
-            trace_dir="traces-$pid"
+        if [[ $pid =~ ^[0-9]+$ ]]; then
+            trace_dir="/workspace/jvm_debug/traces-$pid"
             mkdir -p $trace_dir
             timestamp=$(date +"%Y%m%d_%H%M%S")
             jstack -l $pid > "$trace_dir/stacktrace_$timestamp.txt"
@@ -17,5 +17,5 @@ get_jvm_stack_traces() {
 # Main loop to run the function every 5 seconds
 while true; do
     get_jvm_stack_traces
-    sleep 30
+    sleep 5
 done
