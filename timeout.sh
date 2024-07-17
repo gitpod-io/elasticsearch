15ad6c90590cad3e139bd0c4d17e7cf50a7df6a7
Gero Posmyk-Leinemann <gero@gitpod.io>
2024-07-17 07:10:19 +0000

    stop all tasks

--- timeout.sh
+++ timeout.sh
@@ -1,35 +1,17 @@
 #!/bin/bash
 
-# Function to find parent by name and kill child processes
-find_and_kill_processes() {
-    local binary_name=$1
-
-    # Get the list of PIDs for the given binary name
-    pids=$(pgrep -f "$binary_name")
-
-    for pid in $pids; do
-        # Kill the process and all its children
-        echo "Killing process $pid and its children"
-        pkill -TERM -P $pid
-        #kill -TERM $pid
-    done
-}
-
 # Background task function
 background_task() {
-    local binary_name=$1
     local timeout=$2
 
-    # Wait for the specified timeout
+    echo "timeout.sh: sleeping..."
     sleep $timeout
 
-    # Find and kill the processes
-    find_and_kill_processes "$binary_name"
+    echo "timeout.sh: stopping all tasks"
+    gp tasks stop --all
 }
 
-# Main script execution
-binary_name="remote-dev-server.sh"  # Replace with your binary name
-timeout=60  # Replace with your desired timeout in seconds
+timeout=60
 
 # Start the background task
 background_task "$binary_name" "$timeout" &
