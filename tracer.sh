c053943f5693e6430844327965daab0ef2b350f1
Moritz Eysholdt <moritz@gitpod.io>
2024-09-27 09:37:07 +0000

    troup traces by time and add psforest

--- tracer.sh
+++ tracer.sh
@@ -3,19 +3,26 @@
 
 # Function to get JVM stack traces
 get_jvm_stack_traces() {
+    local trace_dir=$1
     jps | while read -r line; do
         pid=$(echo $line | awk '{print $1}')
         if [[ $pid =~ ^[0-9]+$ ]]; then
-            trace_dir="/workspace/jvm_debug/traces-$pid"
-            mkdir -p $trace_dir
-            timestamp=$(date +"%Y%m%d_%H%M%S")
-            jstack -l $pid > "$trace_dir/stacktrace_$timestamp.txt"
+            jstack -l $pid > "$trace_dir/jstack_$pid.txt"
         fi
     done
 }
 
+get_psforest() {
+    local trace_dir=$1
+    ps -e -a --forest -o pid,ppid,user,args > "$trace_dir/psforest.txt"
+}
+
 # Main loop to run the function every 5 seconds
 while true; do
-    get_jvm_stack_traces
+    timestamp=$(date +"%Y%m%d_%H%M%S")
+    trace_dir="/workspace/jvm_debug/trace-$timestamp"
+    mkdir -p "$trace_dir"
+    get_psforest $trace_dir
+    get_jvm_stack_traces $trace_dir
     sleep 20
 done
